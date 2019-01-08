f7871f2755328dd242dbec4af67ec70741730306
lukasz <robson27-r@hotmail.com>
2019-01-08 11:15:21 +0100

    dodalem plik

---
+++ src/uczesieprogramowania.java
@@ -0,0 +1,11 @@
+public class uczesieprogramowania {
+
+    public static void main(String[] args) {
+
+        System.out.println("ucze");
+        System.out.println("sie");
+        System.out.println("programowania");
+
+
+    }
+}
